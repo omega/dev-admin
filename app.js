584658819f77afe2c5176c38323d9f5d7ff5c1f7
Andreas Marienborg <andreas.marienborg@gmail.com>
2011-07-07 23:29:10 +0700

    More events to keep multiple clients in sync
    We now broadcast most events, so multiple clients can stay in sync. We block more than one script at a time now.
    
    What to do: 
    - Get rid of callback.js, as it is mostly empty now. Might need to re-introduce later, but I hope not.
    - Need to figure out how to specify input (the form) and the variables for the script in a better way

--- app.js
+++ app.js
@@ -12,10 +12,71 @@ var express = require('express')
   , spawn = require("child_process").spawn
   ;
 
+var backlog = new Array(80);
+var bash = spawn_shell();
+
+function spawn_shell() {
+
+    bash = spawn("bash");
+    bash.on("exit", function(code) {
+        console.log("EXIT!");
+        io.sockets.emit("action-done", {'code': code});
+        bash = spawn_shell();
+    });
+    bash.stdout.on("data", function(data) {
+        data.toString().split(/\n/).forEach(function(line) {
+            //backlog.push(line);
+            //while (backlog.length > 80) {
+                //backlog.unshift();
+            //}
+            console.log("emitting stdout: " + line);
+            io.sockets.emit("action-stdout", { text: line });
+        });
+    });
+    bash.stderr.on("data", function(data) {
+        data.toString().split(/\n/).forEach(function(line) {
+            console.log("STDERR: " + line);
+            io.sockets.emit("action-stderr", { text: line });
+        });
+    });
+    bash.run = function(action, params, origin) {
+        bash.origin = origin;
+        bash.running = true;
+        io.sockets.emit('action-new');
+        // Need to send params first!
+        for ( var k in params ) {
+            console.log(k, params[k]);
+            bash.stdin.write(k + "=" + params[k] + "\n");
+            io.sockets.emit('action-variable', { name: k, value: params[k] });
+        }
+        var script = new fs.ReadStream(path.join('actions', action, 'script.sh'));
+        script.on("data", function(data) {
+            data.toString().split(/\n/).forEach(function(line) {
+                console.log("Sending to bash: " + line);
+                if (line.match(/^\#/)) {
+                    bash.origin.emit('action-comment', { text: line });
+                } else {
+                    io.sockets.emit('action-stdin', { text: line });
+                    bash.stdin.write(line + "\n");
+                }
+            });
+        });
+        script.on("end", function() {
+            io.sockets.emit('action-end');
+        });
+    }
+    bash.running = false;
+    return bash;
+}
+
+
+
+
+
 var app = module.exports = express.createServer();
 
 var io = require('socket.io').listen(app);
-
+io.set('log level', 1);
 // Configuration
 
 app.configure(function(){
@@ -44,6 +105,7 @@ app.get('/', function(req, res){
         "actions": DATA.actions
     });
 });
+app.get('/favicon.ico', function(req, res) { res.send() });
 app.get('/:action', function(req, res) {
     res.render(req.params.action, {
         title: 'dev-admin :: ' + req.params.action,
@@ -57,22 +119,26 @@ app.listen(8075);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
 
 io.sockets.on("connection", function(socket) {
-    socket.emit("hello", { hello: "world" });
+    //socket.emit("action-scrollback", { lines: backlog });
+    if (bash.running) {
+        bash.origin.broadcast.emit('action-running');
+    }
     for (var i in DATA.actions) {
         var e = DATA.actions[i];
         console.log(e.name);
         socket.on(e.name, function(data) {
             console.log("Got " + e.name + " event!", data);
-            DATA.actions[e.name].cb(e.name, data, socket);
+            DATA.actions[e.name].cb(e.name, data, socket, bash);
         });
     }
 });
 
+/*
 timers.refresh_data = setInterval(function() {
     console.log("Refreshing dumps and actions");
     refresh_data();
 }, 20000);
-
+*/
 
 function refresh_data() {
     fs.readdir(config.paths.dumps, function(err, files) {
--- public/javascripts/db.js
+++ public/javascripts/db.js
@@ -1,35 +1,51 @@
 $(document).ready(function() {
     var socket = io.connect('/');
-    /*
-    socket.on('hello', function(data) {
-        console.log("Got hello: ", data);
-        socket.emit("reset-db", { dump: 'my-other-dump.tgz' });
+    socket.on("action-done", function(code) {
+        if (code.code == 0) {
+            // All is good
+            $('.console').addClass("ok");
+        } else {
+            $('.console').addClass('error');
+        }
+        $('#log').append("<li class='end'>ACTION EXITED: " + code.code + '</li>');
+        $('form').attr('running', 'stopped');
+        $('.console').removeClass('running');
     });
-    */
+    socket.on("action-stdout", function(data) {
+        console.log("Got stdout!", data);
+        $('#log').append('<li>' + data.text + '</li>');
+    });
+    socket.on("action-stderr", function(data) {
+        $('#log').append('<li class="error">' + data.text + '</li>');
+    });
+    socket.on('action-stdin', function(data) {
+        $('#log').append('<li class="input">' + data.text + '</li>');
+    });
+    socket.on('action-new', function() {
+        $('#log').empty();
+        $('.console').removeClass('ok error');
+        $('.console').addClass('running');
+        $('form').attr('running', 'running');
+    });
+    socket.on('action-variable', function(data) {
+        $('#log').append('<li class="var">' + data.name + "=" + data.value + '</li>');
+    });
+    socket.on('action-running', function() {
+        // We are already in a running shell!
+        $('.console').addClass('running');
+        $('form').attr('running', 'running');
+    });
+
     $('form').submit(function() {
         // XXX: Need to generalize this to support more actions
+        if ($('form').attr('running') == 'running') {
+            console.log("disabled");
+            return false;
+        }
         var dump = { dump: $('#dump').val(), db: "drupal6" }; // XXX: hardcoded db name
         var action = window.location.pathname.match(/^\/([a-z]*)/)[1];
         console.log(action);
         socket.emit(action, dump);
-        $('#log').empty();
-        $('.console').removeClass('ok error');
-        socket.on("action-done", function(code) {
-            if (code.code == 0) {
-                // All is good
-                $('.console').addClass("ok");
-            } else {
-                $('.console').addClass('error');
-            }
-            $('#log').append("<li class='end'>ACTION EXITED: " + code.code + '</li>');
-        });
-        socket.on("action-stdout", function(data) {
-            $('#log').append('<li>' + data.text + '</li>');
-        });
-        socket.on("action-stderr", function(data) {
-            $('#log').append('<li class="error">' + data.text + '</li>');
-        });
-
         return false;
     });
 });
