3430a827b72c63d0dcc0c4d5219d374210be25a8
Andreas Marienborg <andreas.marienborg@gmail.com>
2011-07-08 22:28:15 +0700

    A really dumb script that can be used for testing
    Esp in "prod" mode this can be a good idea :p

---
+++ actions/debug/script.sh
@@ -0,0 +1,8 @@
+# Just a sample action that lets me test and debug some aspects
+
+echo "I am going to sleep for a while now"
+sleep 30
+echo "YAWN. still sleepy. See you in a bit"
+sleep 20
+echo "Aww man, already? ok.."
+
