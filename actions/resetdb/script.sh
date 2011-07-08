d71479467003ac898f1bbc7fd69b3b5139a1ab85
Andreas Marienborg <andreas.marienborg@gmail.com>
2011-07-08 16:30:59 +0700

    Since we support debug mode, turn into a real script :D

--- actions/resetdb/script.sh
+++ actions/resetdb/script.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 # Script to reset a given DB
 
-# XXX: Need a way to specify what data is needed
-echo mysql drop database $db
-sleep 10
-echo mysql create database $db
-sleep 10
-echo "zcat $dump | mysql $db"
-exit 1
+chdir $drupal_path
+
+drush sql-drop -y
+
+zcat $dump | mysql $db
+
+drush status
+drush dbup -y
+
