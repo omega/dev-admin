2a64884fd585adc21f5836b5bd8943cd0354dd06
Andreas Marienborg <andreas.marienborg@gmail.com>
2011-07-08 22:25:35 +0700

    Refactor handlers to reuse them for backlog as well

--- public/javascripts/db.js
+++ public/javascripts/db.js
@@ -1,3 +1,17 @@
+var spitters = {
+    'variable': function(data) {
+        $('#log').append('<li class="var">' + data.name + "=" + data.value + '</li>');
+    },
+    'stdin': function(data) {
+        $('#log').append('<li class="input">' + data.text + '</li>');
+    },
+    'stderr': function(data) {
+        $('#log').append('<li class="error">' + data.text + '</li>');
+    },
+    'stdout': function(data) {
+        $('#log').append('<li>' + data.text + '</li>');
+    }
+};
 $(document).ready(function() {
     var socket = io.connect('/');
     socket.on("action-done", function(code) {
@@ -11,28 +25,31 @@ $(document).ready(function() {
         $('form').attr('running', 'stopped');
         $('.console').removeClass('running');
     });
-    socket.on("action-stdout", function(data) {
-        $('#log').append('<li>' + data.text + '</li>');
-    });
-    socket.on("action-stderr", function(data) {
-        $('#log').append('<li class="error">' + data.text + '</li>');
-    });
-    socket.on('action-stdin', function(data) {
-        $('#log').append('<li class="input">' + data.text + '</li>');
-    });
+    socket.on("action-stdout", spitters.stdout);
+    socket.on("action-stderr", spitters.stderr);
+    socket.on('action-stdin', spitters.stdin);
+    socket.on('action-variable', spitters.variable);
+
     socket.on('action-new', function() {
         $('#log').empty();
         $('.console').removeClass('ok error');
         $('.console').addClass('running');
         $('form').attr('running', 'running');
     });
-    socket.on('action-variable', function(data) {
-        $('#log').append('<li class="var">' + data.name + "=" + data.value + '</li>');
-    });
     socket.on('action-running', function() {
         // We are already in a running shell!
         $('.console').addClass('running');
         $('form').attr('running', 'running');
+    });
+    socket.on('action-scrollback', function(data) {
+        console.log(data);
+        for (var i in data.lines) {
+            var d = data.lines[i];
+            // Need to spit it out
+            if (spitters[d.type]) {
+                spitters[d.type](d);
+            }
+        }
     });
 
     $('form').submit(function() {
