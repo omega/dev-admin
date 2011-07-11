6e4125801cd03a4065036402abb34defb98c05b7
Andreas Marienborg <andreas.marienborg@gmail.com>
2011-07-11 11:33:07 +0700

    Add action-group support

--- app.js
+++ app.js
@@ -39,31 +39,36 @@ function spawn_shell() {
             backlog.push({text: line, type: 'stderr'});
         });
     });
-    bash.run = function(action, params, origin) {
+    bash.run = function(action_config, params, origin) {
+        var group = action_config.group;
+        var action = action_config.name;
+
+        console.log("RUNNING: ", (group ? group : '(groupless)'), action);
         bash.origin = origin;
         bash.running = true;
         io.sockets.emit('action-new');
         // Need to send params first!
-        for ( var k in params ) {
-            bash.stdin.write(k + "=" + params[k] + "\n");
-            io.sockets.emit('action-variable', { name: k, value: params[k], vtype: "var" });
-            backlog.push({name: k, value: params[k], type: 'variable'});
-
-        }
-        // also check for extra params in config!
-        if (config.actions[action]) {
-            for (var k in config.actions[action]) {
-                bash.stdin.write(k + "=" + config.actions[action][k] + "\n");
+        var write_params = function(list, type) {
+            if(!list) return;
+            for (var k in list) {
+                bash.stdin.write(k + "=" + list[k] + "\n");
                 io.sockets.emit('action-variable', {
-                    name: k, value: config.actions[action][k], vtype: 'config'
+                    name: k, value: list[k], vtype: type
                 });
                 backlog.push({
-                    name: k, value: config.actions[action][k], type: 'variable'
+                    name: k, value: list[k], type: 'variable'
                 });
             }
-        }
+        };
+
+        // also check for extra params in config!
+        write_params(params, "var");
+        if (config.action_groups) write_params(config.action_groups[group]);
+        if (config.actions) write_params(config.actions[action]);
+
         bash.stdin.write("set -e\n"); // Enable exit on error!
-        var script = new fs.ReadStream(path.join('actions', action, 'script.sh'));
+        bash.stdin.write("date\n"); // We log the start date, for good show!
+        var script = new fs.ReadStream(path.join(config.paths.actions, (group ? group : ''), action, 'script.sh'));
         script.on("data", function(data) {
             data.toString().split(/\n/).forEach(function(line) {
                 if (line.match(/^\#/)) {
@@ -146,7 +151,7 @@ io.sockets.on("connection", function(socket) {
         (function() {
             var e = DATA.actions[i];
             socket.on(e.name, function(data) {
-                bash.run(e.name, data, socket);
+                bash.run(e, data, socket);
             });
         })();
     }
@@ -160,9 +165,12 @@ timers.refresh_data = setInterval(function() {
 */
 
 function refresh_data() {
-    fs.readdir(config.paths.actions, function(err, folders) {
-        if (err) { return console.error("ERROR reading actions: ", err); }
-
+    read_actions_from_dir(config.paths.actions);
+}
+function read_actions_from_dir(dir, group) {
+    fs.readdir(dir, function(err, folders) {
+        if (err) { return console.error("ERROR reading actions: ", dir, err); }
+        console.log("Scanning ", group, " to look for actions");
         for (i in folders) {
             var name = folders[i];
             // Skip some hidden trash
@@ -170,21 +178,40 @@ function refresh_data() {
             // We do not presently reload actions (require caches anyways)
             if (typeof(DATA.actions[name]) != 'undefined') continue;
 
-            // XXX: Should ad something here for the form/input params!
-            var opts;
+            // If there is no script.sh in the folder, we try to process as
+            // a action group!
+            // XXX: Should prolly not be sync, but for now it is a startup
+            // penalty only
+            var is_action_group = false;;
             try {
-                opts = require(path.join(config.paths.actions, name, 'params.js'));
+                !fs.statSync(path.join(dir, name, 'script.sh')).isFile();
             } catch (e) {
-                console.log("Error loading params for " + name);
+                // this is probably an action group
+                is_action_group = true;
             }
-            DATA.actions[name] = {
-                name: name,
-                opts: opts
+            if (is_action_group) {
+                read_actions_from_dir(path.join(dir, name), name); // name is group
+            } else {
+                var opts;
+                try {
+                    opts = require(path.join(dir, name, 'params.js'));
+                } catch (e) {
+                    console.log("Error loading params for " + name);
+                }
+                console.log("Found action", name, " in group ", group);
+                // XXX: The name needs to be unique across action groups right
+                // now..
+                DATA.actions[name] = {
+                    name: name,
+                    opts: opts,
+                    group: group
+                }
+                // XXX: Need to walk and update any refresh_options calls!
+                //if (typeof(DATA.actions[name].opts.params
             }
-            // XXX: Need to walk and update any refresh_options calls!
-            //if (typeof(DATA.actions[name].opts.params
         }
     });
 }
+
 
 refresh_data();
