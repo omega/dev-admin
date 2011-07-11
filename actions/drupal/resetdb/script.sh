ff8fa108aa0a0ad8773151ca6a0cbe4c0a359615
Andreas Marienborg <andreas.marienborg@gmail.com>
2011-07-11 15:59:43 +0700

    dbup -> updatedb

--- actions/drupal/resetdb/script.sh
+++ actions/drupal/resetdb/script.sh
@@ -2,14 +2,12 @@
 # Script to reset a given DB
 
 cd $drupal_path
-pwd
-env
 
 drush sql-drop -y
 
 zcat $dump | mysql -u root $db
 
-drush dbup -y
+drush updatedb -y
 drush cc all
 drush pm-enable update -y
 drush pm-refresh
