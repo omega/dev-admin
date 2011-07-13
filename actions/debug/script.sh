8ac1bf74e6fe8b51a8c590f4dc6f60e1ea63fd1c
Andreas Marienborg <andreas.marienborg@gmail.com>
2011-07-13 12:00:34 +0700

    Add some ansi testing and ansi parsing in client

--- actions/debug/script.sh
+++ actions/debug/script.sh
@@ -1,5 +1,23 @@
 # Just a sample action that lets me test and debug some aspects
 id
+echo "[1;32;40m[1m[ok][0m"
+echo "[31;40m[1m[error][0m"
+# Show all the colors of the rainbow, should be run under bash
+#for STYLE in 0 1 2 3 4 5 6 7; do
+  #for FG in 30 31 32 33 34 35 36 37; do
+    #for BG in 40 41 42 43 44 45 46 47; do
+      #CTRL="\033[${STYLE};${FG};${BG}m"
+      ##echo -e "${CTRL}"
+      #echo "${STYLE};${FG};${BG}"
+      ##echo -e "\033[0m"
+    #done
+    #echo
+  #done
+  #echo
+#done
+# Reset
+#echo -e "\033[0m"
+
 echo "I am going to sleep for a while now"
 sleep 30
 echo "YAWN. still sleepy. See you in a bit"
--- public/javascripts/db.js
+++ public/javascripts/db.js
@@ -3,13 +3,13 @@ var spitters = {
         $('#log').append('<li class="var">' + data.name + "=" + data.value + '</li>');
     },
     'stdin': function(data) {
-        $('#log').append('<li class="input">' + data.text + '</li>');
+        $('#log').append('<li class="input">' + fix_ansi(data.text) + '</li>');
     },
     'stderr': function(data) {
-        $('#log').append('<li class="error">' + data.text + '</li>');
+        $('#log').append('<li class="error">' + fix_ansi(data.text) + '</li>');
     },
     'stdout': function(data) {
-        $('#log').append('<li>' + data.text + '</li>');
+        $('#log').append('<li>' + fix_ansi(data.text) + '</li>');
     }
 };
 $(document).ready(function() {
@@ -64,6 +64,60 @@ $(document).ready(function() {
     });
 });
 
+    var col_to_class = {
+        30: 'black',
+        31: 'red',
+        32: 'green',
+        33: 'yellow',
+        34: 'blue',
+        35: 'magenta',
+        36: 'cyan',
+        37: 'white'
+    };
+function fix_ansi(intxt) {
+    var txt = intxt.toString(); // just to make sure, and get a copy
+    var ansi = /\x1b\x5b([\d;]+)m/;
+    var res = txt.match(ansi);
+    var cur_fg, cur_bg;
+    var stack = []
+      , flags = { b: false, u: false, bl: false, rev: false, con: false }
+    ;
+    while (res) {
+        var replacement = '';
+        if (res[1] == 0) {
+            while (stack.length) {
+                var e = stack.pop();
+                replacement = replacement + "</span>";
+            }
+        } else {
+            var parts = res[1].split(';');
+            for (var i in parts) {
+                var v = parts[i];
+                var cls = '';
+                if (v < 10) {
+                    // attribute change, ignore for now
+                } else if (v >= 40) {
+                    cls = cls + 'bg' + col_to_class[v - 10];
+                } else {
+                    cls = col_to_class[v];
+                }
+                if (cls != '') {
+                    replacement = replacement + '<span class="' + cls + '">';
+                    stack.push(v);
+                }
+                // XXX: Should really check so we replace other bg and fg
+                // colors in stack!
+            }
+        }
+        txt = txt.replace(res[0], replacement);
 
-
+        res = txt.match(ansi);
+    }
+    // If we have anything in the stack at this point, lets pop and close!
+    if (intxt.toString() != txt) {
+        // We wrap in a global 'ansi' class span
+        txt = '<span class="ansi">' + txt + '</span>';
+    }
+    return txt;
+}
 
