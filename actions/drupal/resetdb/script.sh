369946644f39d6c367017cd7123be358b1c92775
Andreas Marienborg <andreas.marienborg@gmail.com>
2011-07-26 20:45:17 +0700

    SS-128: Add more debug settings

--- actions/drupal/resetdb/script.sh
+++ actions/drupal/resetdb/script.sh
@@ -11,3 +11,13 @@ drush updatedb -y
 drush cc all
 drush pm-enable update -y
 drush pm-refresh
+
+# If you get errors below here, you most likely miss a module:
+# http://drupalcode.org/project/drush_role.git/blob/d727a54:/role.drush.inc
+#
+drush pm-enable devel -yes
+drush vset devel_query_display 1 -yes
+drush role-add-perm --module=devel 1 'access devel information'
+drush role-add-perm --module=devel 2 'access devel information'
+drush vset error_level "1" -yes
+
